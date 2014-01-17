a3e834f1726a3419fec6fbfa10a8bd7eb13e8f79
AcO <AcO>
2014-01-18 00:32:08 +0900

    同時になったどーー！ただしうるさい

--- script/aco.js
+++ script/aco.js
@@ -1,61 +1,46 @@
-$(function() {
-$("#getcontent").on ("click", function(){
-var url ="loop_145.wav"
-b=loadDogSound(url);
-console.log(b);
-playSound(b);
+
+if(typeof(webkitAudioContext)!=="undefined"){
+    var audioctx = new webkitAudioContext();
+}else if(typeof(AudioContext)!=="undefined"){
+    var audioctx = new AudioContext();
+}
+
+$(function(){
+  var buffer = null;
+  console.log(audioctx);
+  LoadSample(audioctx, "./loop_145.wav");
+
+  $("#getcontent").on("click", function(){
+    playMusic();
+  })
 });
-});
 
 
-var context = new webkitAudioContext();
 
-var get_text = function(){
-  var text = $("#text").val();
-  console.log(text);
-  return text;
+
+function playSound(audioctx,buffer,time) {
+    var src = audioctx.createBufferSource();
+    src.buffer = buffer;
+    src.connect(audioctx.destination);
+    src.noteOn(time);
 }
 
-var dogBarkingBuffer = null;
-function loadDogSound(url) {
-  var request = new XMLHttpRequest();
-  request.open('GET', url, true);
-  request.responseType = 'arraybuffer';
-
-  // Decode asynchronously
-  request.onload = function() {
-    context.decodeAudioData(request.response, function(buffer) {
-      dogBarkingBuffer = buffer;
-      console.log(buffer);
-    }, onError);
-  }
-  request.send();
-
-  return dogBarkingBuffer;
+function playMusic(){
+      playSound(audioctx,buffer,0);
+      playSound(audioctx,buffer,3);
 }
-
-
-var context = new webkitAudioContext();
-
-function playSound(buffer) {
-  var source = context.createBufferSource(); // creates a sound source
-  source.buffer = buffer;                    // tell the source which sound to play
-  source.connect(context.destination);       // connect the source to the context's destination (the speakers)
-  source.noteOn(0);                          // play the source now
-}
-
-
-var context = new webkitAudioContext();//一度に４つしかよべない
-
-var ring = function(v){
-    var oscillator = context.createOscillator();
-    oscillator.type = 0;
-    oscillator.frequency.value = v;//周波数
-    oscillator.connect(context.destination);
-    oscillator.noteOn(0);
-    setTimeout(stop,300,oscillator);//関数名,時間,関数名の第一引数　引数つけるときは""をとる
-}
-
-var stop=function(oscillator){
-    oscillator.noteOff(0);
+ 
+function LoadSample(ctx, url) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.responseType = "arraybuffer";
+    req.onload = function() {
+        if(req.response) {
+//          buffer = ctx.createBuffer(req.response, false);
+            ctx.decodeAudioData(req.response,function(b){buffer=b;},function(){});
+        }
+        else
+            buffer = ctx.createBuffer(VBArray(req.responseBody).toArray(), false);
+    }
+    req.send();
 }
