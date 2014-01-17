98b4c5c7127a4eff39b617bad4482ba5b2e6b87a
angie <angie>
2014-01-17 23:46:09 +0900

    とりあえず値がとりたい

---
+++ script/CR_Dicision.js
@@ -0,0 +1,16 @@
+(function(){
+	console.log("test");
+	chord_dicision(FeelingValue.plus[0],FeelingValue.minus[0]);
+});
+
+
+
+//文字をとってくる
+var chord_dicision = function(p_akauri,m_akarui){
+      console.log(p_akauri);
+      console.log("test");
+}
+
+var akarui = function(p_akauri,m_akarui){
+
+}
